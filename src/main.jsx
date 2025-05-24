fecb070e4cd9341683c2f65d3a2817c3c520cdb7
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2025-05-24 19:16:24 +0800

    final comit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import {Test, BoxHead} from './webApp.jsx'
+import {Test, BoxHead, BodyText} from './webApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Test />
     <BoxHead />
+    <BodyText />
   </StrictMode>,
 )
--- src/webApp.jsx
+++ src/webApp.jsx
@@ -8,12 +8,21 @@ function Test(){
 
 function BoxHead() {
   return <div className="center">
-        <div className="button-group">
-            <button className="button">Home</button>
-            <button className="button">About Us</button>
-            <button className="button">Contact Us</button>
-        </div>
+            <div className="button-group">
+                <button className="button">Home</button>
+                <button className="button">About Us</button>
+                <button className="button">Contact Us</button>
+            </div>
     </div>
 }
 
-export { Test, BoxHead};
+function BodyText() {
+    return (
+        <div className="whiteBox">
+            <h1>Test Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    );
+}
+
+export { Test, BoxHead, BodyText};
